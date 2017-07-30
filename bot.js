b81481400126904989521977de72041dbbc1c697
benzarr410 <benzarr410@gmail.com>
2017-07-30 14:59:27 -0400

    Starting a proper rewrite, will soon have each command in a js file (Hopefully)

--- bot.js
+++ bot.js
@@ -3,10 +3,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const sleep = require('system-sleep');
 const fs = require('fs');
+const config = require('./config.json');
 var Quotes = []
 var Fstrings = [" with a transformer.", ", but creates a black hole and gets sucked in.", " with poutine.", ", but they slipped on a banana peel", " and in the end, the only victor was the coffin maker.", ", and what a fight it is!  Whoa mama!", ", with two thousand blades!", ", but he fell into a conveniently placed manhole!, but they tripped over a rock and fell in the ocean.", ", but they hurt themselves in their confusion.", ". SHORYUKEN!", ". HADOUKEN!", ". KA-POW!", " with a pillow.", " with a large fish.", ", but they stumbled over their shoelaces.", ", but they missed.", " with a burnt piece of toast.", ", but it wasn't very effective..."];
 var rand = Fstrings[Math.floor(Math.random() * Fstrings.length)];
-var dadmode = 0
+var dadmode = (config.dadmode)
 String.prototype.toAlternatingCase = function () {
     tmp = this.toLowerCase(); (this[0] == this[0].toLowerCase() ? i = 1 : i = 0); for(i; i < tmp.length; i = i+2) { tmp = tmp.substr(0,i) + tmp[i].toUpperCase() + tmp.substr(i+1); } return tmp;
 }
@@ -18,10 +19,10 @@ function randNum(min, max) {
 }
 //Boot Sequence
 client.on('ready', () => {
-  console.log('Ready!');
+  console.log('Ready!')
 });
 //The Good Shit
-var prefix = "g-"
+var prefix = "p."
 //Ping command
 client.on('ready', () => {
         client.user.setGame('Being Coded by Gallium#1327...slowly');
@@ -49,7 +50,7 @@ else if (message.content.startsWith(prefix + 'pressF')) {
   if (message.content === (prefix + 'help')
 ) {
     message.react('👌');
-    message.author.send("HELP:\ng-help: This, of course.\ng-ping: Shows your ping.\ng-pressF: Pays Respects to a user, defaults to you if no user is mentioned.\ng-quote: Pull a quote.\ng-storequote: Store a quote to pull later.\ng-coin: Flip a coin.\ng-spongemock <text>: MoCk SoMe TeXt\ng-fight @user: Fight someone, ripped straight from Bug-Bot\ng-guildinfo: Learn about the guild you're in. **REQUIRES EMBED LINKS**\ng-spinner: Spin a fidget spinner.\ng-about: Learn more about me!")
+    message.author.send("HELP:\np.help: This, of course.\np.ping: Shows your ping.\np.pressF: Pays Respects to a user, defaults to you if no user is mentioned.\np.quote: Pull a quote.\np.storequote: Store a quote to pull later.\np.coin: Flip a coin.\np.spongemock <text>: MoCk SoMe TeXt\np.fight @user: Fight someone, ripped straight from Bup.Bot\np.guildinfo: Learn about the guild you're in. **REQUIRES EMBED LINKS**\np.spinner: Spin a fidget spinner.\np.about: Learn more about me!")
 }
 Array.prototype.randomElement = function (array) {
     return array[Math.floor(Math.random() * array.length)]
@@ -99,7 +100,7 @@ else if (message.mentions.users.size > 1) {
 }
   else {
     if (message.channel.type === "dm") {
-      message.channel.send(message.author.username + " is fighting GalliumBot" + Fstrings.randomElement(Fstrings));
+      message.channel.send(message.author.username + " is fighting Paradox" + Fstrings.randomElement(Fstrings));
     }
     else {
 message.channel.send(message.author.username + " is fighting " + message.mentions.users.first().username + Fstrings.randomElement(Fstrings));
@@ -137,7 +138,7 @@ if (message.content === (prefix + "guildinfo")) {
 message.channel.send({embed})
 }}
 if (message.content === (prefix + 'about')){
-message.channel.send(":wave: **Hi there!** :smiley: \n\nThis bot is made by Gallium#1327, and the code can be found at https://github.com/benzarr410/GalliumBot. It's also being further developed by Akii#2111 so that it fits what their server needs, so check out their repo too at https://github.com/jennasisis/GalliumBot. \n\nYou can find all the commands for this bot by typing ``g-help``. **Remember, this bot is still in development.** So most of its features may still be buggy. If you encounter any problems, please feel free to contact Gallium#1327. \n\n**Thanks for using the bot!**")
+message.channel.send(":wave: **Hi there!** :smiley: \n\nThis bot is made by Gallium#1327, and the code can be found at https://github.com/benzarr410/GalliumBot. It's also being further developed by Akii#2111 so that it fits what their server needs, so check out their repo too at https://github.com/jennasisis/GalliumBot. \n\nYou can find all the commands for this bot by typing ``p.help``. **Remember, this bot is still in development.** So most of its features may still be buggy. If you encounter any problems, please feel free to contact Gallium#1327. \n\n**Thanks for using the bot!**")
 }
 if(message.content.startsWith(prefix + "shame")) {
  if (message.mentions.users.size < 1) {
@@ -152,7 +153,7 @@ message.channel.send(message.mentions.users.first() + ", 🔔 ***S H A M E*** �
 }
 if (message.content.startsWith(prefix + "spinner")) {
  if (message.content == prefix + "spinner") {
-   message.channel.send("Available spinners: Red, Orange, Yellow, Green, Blue, Purple, Space\n\nSpin one of these with `g-spinner <type>`")
+   message.channel.send("Available spinners: Red, Orange, Yellow, Green, Blue, Purple, Space\n\nSpin one of these with `p.spinner <type>`")
  }
  else {
   var spinnerType = message.content.substring(10)
@@ -195,7 +196,7 @@ else if (message.content.substring(10) === "on") {
   message.channel.send("Dad mode on. Send a message starting with **I'm** to begin.")
 }
   else {
-    message.channel.send("I couldn't understand. If you would like to turn dad mode off, type `g-dadmode off`. If you want to turn it back on, type `g-dadmode on`.")
+    message.channel.send("I couldn't understand. If you would like to turn dad mode off, type `p.dadmode off`. If you want to turn it back on, type `p.dadmode on`.")
   }
   }
 if (message.content.startsWith("I'm") && dadmode == "1") {
@@ -220,4 +221,4 @@ if (message.content.startsWith(prefix + "temp")) {
 }
 });
 //Token
-client.login("TOKEN");
+client.login(config.token);
